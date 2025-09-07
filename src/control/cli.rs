17f67a2f4cfadebddfc086663b79f7987908cf0a
Benjamin West <ben.west.500@gmail.com>
2025-09-07 13:19:34 +0100

    fix: upload to crates and some docstrings

--- src/control/cli.rs
+++ src/control/cli.rs
@@ -1,10 +1,11 @@
 use std::io::{stderr, stdin, stdout, Stderr, Stdin, Stdout, Write};
 use crate::{units, constants};
 
+/// Prompt given to the user for each command
 const PROMPT: &str = "Command> ";
 
 #[derive(Debug, Clone, Copy)]
-/// 
+/// Gettable pieces of data from the Reactor
 enum GetParams {
     Temperature,
     RemainingFuel,
@@ -12,12 +13,15 @@ enum GetParams {
 }
 
 #[derive(Debug, Clone, Copy)]
+/// The CoreCommand enum which is passed through the channels between 
+/// `cli` and the `reactor`
 pub enum CoreCommand {
     Scram,
     Get(GetParams),
     MoveRods(units::RodPosition),
 }
 
+/// Main code for the users cli thread
 pub fn run_cli() {
     let sin: Stdin = stdin();
     let mut sout: Stdout = stdout();
--- src/control/mod.rs
+++ src/control/mod.rs
@@ -3,6 +3,11 @@ pub mod cli;
 
 use scada::Scada;
 
+/// Represents the command and control side of the reactor sim
+/// It is concerned with:
+/// * Running the CLI thread
+/// * Logging reactor state (temps, pressures etc.)
+/// * Detecting dangerous conditions and auto-regulation of reactor 
 pub struct ControlRoom {
     scada: Scada,
 }
